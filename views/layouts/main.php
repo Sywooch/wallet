36bc0e0fc644b027cb6565c1c8afbd496b5fe77c
alexshadie <alex@astra.ws>
2015-07-16 01:28:31 +0600

    Tags sketch

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,6 +36,7 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     ['label' => 'Home', 'url' => ['/site/index']],
+                    ['label' => 'Tags', 'url' => ['tags']],
                     ['label' => 'About', 'url' => ['/site/about']],
                     ['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
---
+++ views/tags/index.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'My Wallet - Tags';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Tags</h1>
+    </div>
+
+    <div class="body-content">
+
+    </div>
+</div>

0ddd4f827b5b16fc1ecf1a44869470904bda3a7b
alexshadie <alex@astra.ws>
2015-08-10 20:00:16 +0600

    db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=wallet',
+    'dsn' => 'mysql:host=letris.ru;dbname=wallet',
     'username' => 'wallet',
-    'password' => 'wallet',
+    'password' => 'shadiewalletpass',
     'charset' => 'utf8',
 ];
